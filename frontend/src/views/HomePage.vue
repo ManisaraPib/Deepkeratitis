ca884404b2a385bae93a0884796f9bc4ee8a49b3
Sirakis Ngamwongwan <chojaokun@gmail.com>
2022-09-03 16:15:14 +0700

    Update HomePage.vue

--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -26,6 +26,7 @@
             Grad cam
           </div>
           <img :src="imageAfter" class="my-3 my-img" />
+          <img src="../../../backend/img/result.jpg" class="my-3 my-img" />
         </div>
       </div>
       <div class="left18"></div>
@@ -33,10 +34,18 @@
         <div class="button-center">
           <button class="predict-button" @click="back">
             Back
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
-              class="bi bi-arrow-left-short" viewBox="0 0.5 16 16">
-              <path fill-rule="evenodd"
-                d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="currentColor"
+              class="bi bi-arrow-left-short"
+              viewBox="0 0.5 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"
+              />
             </svg>
           </button>
         </div>
